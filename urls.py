883da8afcf02122db8e4671992c0d3dac127c527
Abhilash <abhilash@Abhilash-EC2.(none)>
2012-09-15 18:28:14 +0530

    URL Added

--- urls.py
+++ urls.py
@@ -4,14 +4,29 @@ from autoregister import autoregister
 from django.contrib import admin
 admin.autodiscover()
 autoregister('main')
+autoregister('front_end')
 urlpatterns = patterns('main',
     # ============= MAIN URL ===============================
     url(r'^$', 'views.index', name='index'),
     url(r'^home/$', 'views.home', name='home'),
     url(r'^logout/$', 'views.logout_view', name='logout'),
-    url(r'^movie/(?P<movie_id>.*)/$','views.movie_info',name='movie'),
-    
+    url(r'^movie/(?P<movie_id>.*)/$','views.movie_info',name='movie'),    
     # =========== ADMIN URL ================================
     url(r'^admin/', include(admin.site.urls)),
 )
 
+rlpatterns += patterns('front_end',
+      #TEmplate view
+      url(r'^new_account$','views.register_view',name='register_view'),
+      #From submit
+      url(r'^register$','views.register',name='register'),
+      url(r'^home/myaccount/$','views.myaccount',name='myaccount'),
+      url(r'^home/profile/(?P<user_id>.*)/$', 'views.myaccount', name='user_page'),
+      url(r'user/request/(?P<user_id>.*)/$', 'views.friend_request', name='friend_request'),
+      url(r'user/accept/(?P<user_id>.*)/$', 'views.friend_accept', name='friend_accept'),
+      url(r'^home/myaccount/edit_profile_page/$','views.edit_profile_page',name='edit_profile_page'),
+      url(r'home/myaccount/edit_profile_page/delete-pic/$', 'views.delete_pic', name='delete_pic'),
+      url(r'home/myaccount/edit_profile_page/profile-pic/$', 'views.profile_pic', name='profile_pic'),
+      
+
+)
