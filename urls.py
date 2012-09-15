b2f02a97fc93c2cbf126a8707a0bc7960d3bd51a
Abhilash <abhilash@Abhilash-EC2.(none)>
2012-09-15 18:02:58 +0530

    Test

--- urls.py
+++ urls.py
@@ -3,8 +3,15 @@ from autoregister import autoregister
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
-
-urlpatterns = patterns('',
+autoregister('main')
+urlpatterns = patterns('main',
+    # ============= MAIN URL ===============================
+    url(r'^$', 'views.index', name='index'),
+    url(r'^home/$', 'views.home', name='home'),
+    url(r'^logout/$', 'views.logout_view', name='logout'),
+    url(r'^movie/(?P<movie_id>.*)/$','views.movie_info',name='movie'),
+    
+    # =========== ADMIN URL ================================
     url(r'^admin/', include(admin.site.urls)),
 )
 
