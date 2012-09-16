4e9c67e82101cb3aa58796c78ee65ba81a4ad6cc
Abhilash <abhilash@Abhilash-EC2.(none)>
2012-09-16 21:25:34 +0530

    Contxt updated

--- context_processors.py
+++ context_processors.py
@@ -1,8 +1,8 @@
 from front_end.models import *
 
 def menu(request):
-    navigations = Navigation.objects.filter(publish=True)
-    members = Team_Members.objects.filter(publish=True)
-    boxoffice_movies =Boxoffice.objects.order_by('-imdbid__rating')[:7]
+	navigations = Navigation.objects.filter(publish=True)
+	members = Team_Members.objects.filter(publish=True)
+	boxoffice_movies =Boxoffice.objects.order_by('-imdbid__rating')[:7]
 	intheater_movies =Intheaters.objects.order_by('-imdbid__rating')[:7]
-    return {'navigations' : navigations,'members':members ,'boxoffice_movies':boxoffice_movies,'intheater_movies':intheater_movies}
+	return {'navigations' : navigations,'members':members ,'boxoffice_movies':boxoffice_movies,'intheater_movies':intheater_movies}
