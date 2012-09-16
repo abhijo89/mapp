13da0ad194413b6bf09553e4fbaf49736575e01f
Abhilash <abhilash@Abhilash-EC2.(none)>
2012-09-16 10:06:16 +0530

    Updated app name

--- main/templatetags/movie_tags.py
+++ main/templatetags/movie_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from django.conf import settings
-from movie.main.models import *
+from mapp.main.models import *
 from front_end.models import *
 
 register = template.Library()
