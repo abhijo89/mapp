29bfc58eb435bd9511a10f80e9f2d3f21d0336db
Abhilash <abhilash@Abhilash-EC2.(none)>
2012-09-17 07:26:45 +0530

    Test

--- main/views.py
+++ main/views.py
@@ -44,6 +44,8 @@ def home(request,template='main/index.html'):
 	if request.user.is_anonymous():
 		return HttpResponseRedirect(reverse('index'))
 	today = datetime.date.today()
+	upcomming_movies =Upcoming.objects.order_by('-imdbid__rating')[:7]
+	opening_movies =Opening.objects.order_by('-imdbid__rating')[:7]
 	context = {}
 	return render_to_response('main/index.html', context, context_instance = RequestContext(request))
 
