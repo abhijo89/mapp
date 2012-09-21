2aa3b317d319cb2e7b4542a26db7b38914110623
abhijo89 <abhilash@mangaloreinfotech.in>
2012-09-21 18:38:39 +0530

    Setting up ther fields

--- tracking/admin.py
+++ tracking/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from tracking.models import BannedIP, UntrackedUserAgent,Visitor
 class VisitorAdmin(admin.ModelAdmin):
-	list_display = ('ip_address','user','referrer','url','page_views')
+	list_display = ('ip_address','user','referrer','url','page_views','user_agent','session_star','last_update')
 admin.site.register(BannedIP)
 admin.site.register(UntrackedUserAgent)
 admin.site.register(Visitor,VisitorAdmin)
