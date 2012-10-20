376d71958357b957515f33f6944baa2c0321f8af
abhijo89 <abhilash@mangaloreinfotech.in>
2012-10-20 16:51:45 +0530

    Get total visitors count

--- context_processors.py
+++ context_processors.py
@@ -1,5 +1,5 @@
 from front_end.models import *
-from tracking.models import import *
+from tracking.models import  *
 from main.models import Boxoffice,Intheaters,Upcoming,Opening
 
 def menu(request):
@@ -10,5 +10,5 @@ def menu(request):
 	intheater_movies =Intheaters.objects.order_by('-imdbid__rating')[:7]
 	upcomming_movies =Upcoming.objects.order_by('-imdbid__rating')[:10]
 	opening_movies =Opening.objects.order_by('-imdbid__rating')[:10]
-	#visitors = Visitor.objects.count()
+	visitors = Visitor.objects.count()
 	return {'navigations' : navigations,'members':members ,'boxoffice_movies':boxoffice_movies,'intheater_movies':intheater_movies,'upcomming_movies':upcomming_movies,'opening_movies':opening_movies,'visitors':visitors}
